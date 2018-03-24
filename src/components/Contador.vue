caea14ae35957cb87859ee56e75976de66a55461
PatriciaMachado <ppl.machado@gmail.com>
2018-03-24 16:44:02 -0300

    alteracao contador

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -6,6 +6,7 @@
     <button type="button" @click="subtrairValor()">Subtrair</button>
     <p>{{parOuImpar}}</p>
     <p>{{numeroPrimo}}</p>
+    <p>{{negativoOuPositivo}}</p>
   </div>
 </template>
 
@@ -23,16 +24,19 @@ export default {
       return 'IMPAR';
     },
     numeroPrimo() {
-      for (var i = 2; i < this.value; i++) {
+      for (const i = 2; i < this.value; i + 1) {
         if (this.value % i === 0) {
-        return 'NAO PRIMO';
+          return 'NAO PRIMO';
         }
       }
       return this.value > 1;
     },
     negativoOuPositivo() {
-
-    }
+      if (this.valor > 0) {
+        return 'POSITIVO';
+      }
+      return 'NEGATIVO';
+    },
   },
   methods: {
     agregarValor() {
